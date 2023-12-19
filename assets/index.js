d1eb62c6db699ca9a82b8fc71b0151ecdae0592f
421yl <irene030325@gmail.com>
2023-12-20 04:55:29 +0800

    第五次註解

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,33 @@
-let slideIndex = 0;
+let slideIndex = 1; //先設預設變數為1
 
-        function showSlides(props) {
-            let i;
-            let slides = document.getElementsByClassName("singleSlide");
+function showSlides(props) {
+  let i;
+  //取得所有的mySlides 因為他是取class 而不是id 所以要用getElementsByClassName
+  let slides = document.getElementsByClassName("singleSlide");
 
-            if (props > slides.length) {
-                slideIndex = 1;
-            }
+  //如果n大於slides的長度，代表已經到最後一張，所以要從第一張開始
+  if (props > slides.length) {
+    slideIndex = 1;
+  }
 
-            if (props < 1) {
-                slideIndex = slides.length;
-            }
+  if (props < 1) {
+    slideIndex = slides.length;
+  }
 
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+  //將所有圖片隱藏
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-            slides[slideIndex - 1].style.display = "flex";
-        }
+  //將新的圖片顯示
+  //console.log(slideIndex - 1, 25);
 
-        function plusSlides(a) {
-            showSlides((slideIndex += a));
-        }
+  //array都從0開始，所以要減一，因為我們的slideIndex是從1開始 ，已經賦予值
+  slides[slideIndex -1].style.display = "flex";
+}
 
-        showSlides(slideIndex); // 在這裡調用 showSlides 開始顯示第一張圖片
-        plusSlides(0); // 在這裡調用 plusSlides，將初始顯示的三張圖片更正
+function plusSlides(a) {
+  showSlides((slideIndex += a));
+}
+//使用者點擊左右按鈕時，並傳入參數1或-1，代表下一張或上一張
+plusSlides(slideIndex);
