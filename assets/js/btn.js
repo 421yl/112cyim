696c17bcd4e061e51d75d66d2238e95c9bcb7606
421yl <irene030325@gmail.com>
2023-12-21 09:06:13 +0800

    第15次註解

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -1,22 +1,21 @@
-var num_jia = document.getElementById("num-jia");
-var num_jian = document.getElementById("num-jian");
-var input_num = document.getElementById("input-num");
+var num_jia_buttons = document.querySelectorAll(".num-jia");
+var num_jian_buttons = document.querySelectorAll(".num-jian");
+var input_nums = document.querySelectorAll(".input-num");
 
-num_jia.onclick = function() {
+num_jia_buttons.forEach(function(button) {
+    button.onclick = function() {
+        input_nums.forEach(function(input_num) {
+            input_num.value = parseInt(input_num.value) + 1;
+        });
+    };
+});
 
-    input_num.value = parseInt(input_num.value) + 1;
-
-}
-
-num_jian.onclick = function() {
-
-    if(input_num.value <= 0) {
-        input_num.value = 0;
-    } 
-    
-    else {
-
-        input_num.value = parseInt(input_num.value) - 1;
-    }
-
-}
+num_jian_buttons.forEach(function(button) {
+    button.onclick = function() {
+        input_nums.forEach(function(input_num) {
+            if (input_num.value > 0) {
+                input_num.value = parseInt(input_num.value) - 1;
+            }
+        });
+    };
+});
